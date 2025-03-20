25e44c7650e8e324d39ea45fdf5c2ba5d5a77c99
RaphaelLuccas <raphaelluccasdev@gmail.com>
2025-03-19 21:50:53 -0300

    Added practice on object-oriented programming in Java, focusing on object creation, initialization, and attributes.

---
+++ src/NivelIntermediario/Main.java
@@ -0,0 +1,25 @@
+package NivelIntermediario;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Criar ninja Naruto Uzumaki - Naruto é m objeto
+        Ninja Naruto = new Ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.aldeia = "Aldeia da Folha";
+        Naruto.idade = 17;
+
+        //Criar ninja Sasuke Uchiha - Sasuke é um objeto
+        Ninja Sasuke = new Ninja();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.aldeia = "Aldeia da folha";
+        Sasuke.idade = 18;
+
+        //Criar ninja Sakura Haruno - Sakura é um objeto
+        Ninja Sakura = new Ninja();
+        Sakura.nome = "Sakura Haruno";
+        Sakura.aldeia = "Aldeia da folha";
+        Sakura.idade = 19;
+
+    }
+}
